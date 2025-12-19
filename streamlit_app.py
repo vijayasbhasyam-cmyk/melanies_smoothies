cc31d1cce149bcaf7ad5a2a043b411b32ba14593
vijayasbhasyam-cmyk <vijayasbhasyam@gmail.com>
2025-12-19 18:52:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,10 +27,10 @@ if ingredients_list:
     #st.text (ingredients_list)
 
     ingredients_string=''
-    for fruit_choosen in ingredients_list:
-        ingredients_string +=fruit_choosen + ' '
-        st.subheader(fruit_choosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_choosen)
+    for fruit_chosen in ingredients_list:
+        ingredients_string +=fruit_chosen + ' '
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
