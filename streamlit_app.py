178947fa6056cdc4dacf47cbe8f5679604439d9f
vijayasbhasyam-cmyk <vijayasbhasyam@gmail.com>
2025-12-19 18:47:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,6 +29,8 @@ if ingredients_list:
     ingredients_string=''
     for fruit_choosen in ingredients_list:
         ingredients_string +=fruit_choosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe( data=smoothiefroot_response.json(),use_container_width=True)
     #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
@@ -43,8 +45,7 @@ if ingredients_list:
         st.success('Your Smoothie is ordered,'+name_on_order +'!', icon="✅")
 
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-sf_df = st.dataframe( data=smoothiefroot_response.json(),use_container_width=True)
+
 
 
 
